0e2e5241f3a7d2168ad87e6acfff9312df926f7f
emestiza <emestiza@u.rochester.edu>
2020-08-04 20:47:45 -0400

    Day 3

--- app.js
+++ app.js
@@ -1,20 +1,131 @@
 //SET URL SO IT USES DEPLOYED API URL IF IT EXISTS, LOCALHOST IF IT DOESN'T
-const deployedURL = "https://job-tracker-emestiza.herokuapp.com/";
+const deployedURL = "https://job-tracker-emestiza.herokuapp.com";
 const URL = deployedURL ? deployedURL : "http://localhost:3000";
 
 //GLOBAL VARIABLES
-// const $createButton = $("#create");
-// const $updateButton = $("#update");
-// const $deleteButton = $("#delete");
+const $createButton = $("#create");
+const $updateButton = $("#update");
+const $deleteButton = $("#delete");
 
+//FUNCTIONS
+$.getJSON("https://job-tracker-emestiza.herokuapp.com", function(data) {
+    $(data).each(function(index, job) {
+        $('#jobBody').append($("<tr>")
+        .append($("<td>").append(job.company))
+        .append($("<td>").append(job.position))
+        .append($("<td>").append(job.location))
+        .append($("<td>").append(job.date))
+        .append($("<td>").append(job.phone))
+        .append($("<td>").append(job.onsite))
+        .append($("<td>").append(job.offer))
+        .append($("<td>").append(job.url))
+        );
+    });
+});
+  
+// Get all jobss
+const getJob = async () => {
+    // Gets the jobs
+    const response = await fetch(`${URL}/job`);
+    const data = await response.json();
+    console.log(data)
+    // Populate DOM with jobs
+    data.forEach((job) => {
+		// Append table with add row form on add new button click
+        $(".add-new").click(function(){
+            $(this).attr("disabled", "disabled");
+            const index = $("table tbody tr:last-child").index();
+            const row = '<tr>' +
+                '<td><input type="text" class="form-control" name="company" id="company"></td>' +
+                '<td><input type="text" class="form-control" name="position" id="position"></td>' +
+                '<td><input type="text" class="form-control" name="location" id="location"></td>' +
+                '<td><input type="text" class="form-control" name="date" id="date"></td>' +
+                '<td><input type="text" class="form-control" name="phone" id="phone"></td>' +
+                '<td><input type="text" class="form-control" name="onsite" id="onsite"></td>' +
+                '<td><input type="text" class="form-control" name="offer" id="offer"></td>' +
+                '<td><input type="text" class="form-control" name="url" id="url"></td>' +
+                '<td>' + actions + '</td>' +
+            '</tr>';
+            $("table").append(row);		
+            $("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    })
+};
+
+getJob();
+  
+  // CREATE A RAT
+  const createRat = async () => {
+    //Create to New Rat from Form Data
+    const newRat = {
+      name: $nameInput.val(),
+      pizza: $pizzaSelect.val(),
+    };
+    //Send request to api to create rat
+    const response = await fetch(`${URL}/rats`, {
+      method: "post",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(newRat)
+    });
+    const data = response.json();
+    //update the DOM
+    $ul.empty();
+    getRats();
+  };
+  
+  // DELETE
+  const deleteRat = async (event) => {
+    const response = await fetch(`${URL}/rats/${event.target.id}`, {method: "delete"});
+  
+     //update the DOM
+     $ul.empty();
+     getRats();
+  }
+  
+  //UPDATE
+  //Update a Rat
+  const updateRat = async (event) => {
+    //Logging the event object
+    console.log(event)
+    //Create Updated Rat Object
+    const updatedRat = {
+      name: $nameEditInput.val(),
+      pizza: $pizzaEditSelect.val()
+    }
+    //make our put request
+    const response = await fetch(`${URL}/rats/${event.target.id}`, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(updatedRat)
+    })
+    //update the dom
+    $ul.empty();
+    getRats();
+  }
+  
+  // Main Application Logic
+  // Start executing below
+  
+  //initially get existing jobs
+  getJob();
+  //add create function to button click
+  $button.on('click', createRat);
+  //add update function to edit submit button
+  $editButton.on("click", updateRat)
+
+
+// Table Actions
 $(document).ready(function(){
 	$('[data-toggle="tooltip"]').tooltip();
-	let actions = $("table td:last-child").html();
+	const actions = $("table td:last-child").html();
 	// Append table with add row form on add new button click
     $(".add-new").click(function(){
 		$(this).attr("disabled", "disabled");
-		let index = $("table tbody tr:last-child").index();
-        let row = '<tr>' +
+		const index = $("table tbody tr:last-child").index();
+        const row = '<tr>' +
             '<td><input type="text" class="form-control" name="company" id="company"></td>' +
             '<td><input type="text" class="form-control" name="position" id="position"></td>' +
             '<td><input type="text" class="form-control" name="location" id="location"></td>' +
@@ -31,8 +142,8 @@ $(document).ready(function(){
     });
 	// Add row on add button click
 	$(document).on("click", ".add", function(){
-		let empty = false;
-		let input = $(this).parents("tr").find('input[type="text"]');
+		const empty = false;
+		const input = $(this).parents("tr").find('input[type="text"]');
         input.each(function(){
 			if(!$(this).val()){
 				$(this).addClass("error");
@@ -65,3 +176,11 @@ $(document).ready(function(){
     });
 });
 
+
+
+// Hamburger Toggle
+jQuery(function($){
+    $( '.navbar.navbar-4' ).click(function(){
+    $('.navbar-toggler-icon').toggleClass('expand')
+    })
+ })
