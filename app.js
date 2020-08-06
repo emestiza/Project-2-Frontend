e3985c34ec2698e7bf576879298e225c3459c615
emestiza <emestiza@u.rochester.edu>
2020-08-06 15:03:54 -0400

    Day 5

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const getJob = async () => {
       $tr.append($('<td>').text(`${job.onsite}`));
       $tr.append($('<td>').text(`${job.offer}`));
       $tr.append($('<td>').text(`${job.url}`));
-        const $updateButton = $('<button>').text("Update").addClass("update").attr("id", job._id).attr("data-toggle","modal").attr("data-target", "modal" + `${job._id}`)
+      const $updateButton = $('<button>').text("Update").addClass("update").attr("id", job._id).attr("data-toggle","modal").attr("data-target", "modal" + `${job._id}`)
       $tr.append($('<td>').append($updateButton));
       const $deleteButton = $('<button>').text("Delete").addClass("delete").attr("id", job._id);
       $tr.append($('<td>').append($deleteButton));
@@ -161,64 +161,3 @@ function openModal(job){
   $createButton.on('click', createJob);
   $saveButton.on('click', updateJob);
 
-
-
- // Post MVP 
-// Table Actions
-// $(document).ready(function(){
-// 	$('[data-toggle="tooltip"]').tooltip();
-// 	const actions = $("table td:last-child").html();
-// 	// Append table with add row form on add new button click
-//     $(".add-new").click(function(){
-// 		$(this).attr("disabled", "disabled");
-// 		const index = $("table tbody tr:last-child").index();
-//         const row = '<tr>' +
-//             '<td><input type="text" class="form-control" name="company" id="company"></td>' +
-//             '<td><input type="text" class="form-control" name="position" id="position"></td>' +
-//             '<td><input type="text" class="form-control" name="location" id="location"></td>' +
-//             '<td><input type="text" class="form-control" name="date" id="date"></td>' +
-//             '<td><input type="text" class="form-control" name="phone" id="phone"></td>' +
-//             '<td><input type="text" class="form-control" name="onsite" id="onsite"></td>' +
-//             '<td><input type="text" class="form-control" name="offer" id="offer"></td>' +
-//             '<td><input type="text" class="form-control" name="url" id="url"></td>' +
-// 			'<td>' + actions + '</td>' +
-//         '</tr>';
-//     	$("table").append(row);		
-// 		$("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
-//         $('[data-toggle="tooltip"]').tooltip();
-//     });
-// 	// Add row on add button click
-// 	$(document).on("click", ".add", function(){
-// 		const empty = false;
-// 		const input = $(this).parents("tr").find('input[type="text"]');
-//         input.each(function(){
-// 			if(!$(this).val()){
-// 				$(this).addClass("error");
-// 				empty = true;
-// 			} else{
-//                 $(this).removeClass("error");
-//             }
-// 		});
-// 		$(this).parents("tr").find(".error").first().focus();
-// 		if(!empty){
-// 			input.each(function(){
-// 				$(this).parent("td").html($(this).val());
-// 			});			
-// 			$(this).parents("tr").find(".add, .edit").toggle();
-// 			$(".add-new").removeAttr("disabled");
-// 		}		
-//     });
-// 	// Edit row on edit button click
-// 	$(document).on("click", ".edit", function(){		
-//         $(this).parents("tr").find("td:not(:last-child)").each(function(){
-// 			$(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
-// 		});		
-// 		$(this).parents("tr").find(".add, .edit").toggle();
-// 		$(".add-new").attr("disabled", "disabled");
-//     });
-// 	// Delete row on delete button click
-// 	$(document).on("click", ".delete", function(){
-//         $(this).parents("tr").remove();
-// 		$(".add-new").removeAttr("disabled");
-//     });
-// });
